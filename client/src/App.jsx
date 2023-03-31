50eb0239788721677b340353d8f3134c9da1663a
swapy96 <s.swapnil80@gmail.com>
2023-03-31 12:23:42 -0700

    template1 completed - style is remaining

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,12 +4,13 @@ import Footer from "./components/Footer/Footer";
 import HomePage from "./pages/HomePage";
 import NotFoundPage from "./pages/NotFoundPage";
 import Aside from "./components/Aside/Aside";
+import TemplateFirstPage from "./components/TemplateFirstPage/TemplateFirstPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Aside />}></Route>
+        <Route path="/" element={<TemplateFirstPage />}></Route>
         <Route path="/home" element={<HomePage />}></Route>
         <Route path="*" element={<NotFoundPage />}></Route>
       </Routes>
---
+++ client/src/components/TemplateFirstPage/TemplateFirstPage.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./TemplateFirstPage.scss";
+import img1 from "../../assets/imgTemplateDashbord/D1.svg";
+import img2 from "../../assets/imgTemplateDashbord/D2.svg";
+import img3 from "../../assets/imgTemplateDashbord/D3.svg";
+import img4 from "../../assets/imgTemplateDashbord/D4.svg";
+
+function TemplateFirstPage() {
+  return (
+    <section>
+      <h1>Dashboard Templates</h1>
+      <div className="container">
+        <div>
+          <img src={img1} alt="img1" />
+        </div>
+        <div>
+          <img src={img2} alt="img2" />
+        </div>
+        <div>
+          <img src={img3} alt="img3" />
+        </div>
+        <div>
+          <img src={img4} alt="img4" />
+        </div>
+      </div>
+      <button>Apply Template</button>
+    </section>
+  );
+}
+
+export default TemplateFirstPage;
