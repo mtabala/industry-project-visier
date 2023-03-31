1762989865b9d0e8b22eb229c33e6195ddf12c26
swapy96 <s.swapnil80@gmail.com>
2023-03-31 10:44:40 -0700

    completes the html part for aside of dashboard

---
+++ client/src/components/Aside/Aside.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Aside() {
+  return (
+    <aside className="aside">
+      <nav className="aside__nav">
+        <ul className="aside__list">
+          <li className="aside__item">Onboarding</li>
+          <li className="aside__item">Employee</li>
+          <li className="aside__item">Performance</li>
+          <li className="aside__item">Engagement</li>
+        </ul>
+      </nav>
+    </aside>
+  );
+}
+
+export default Aside;
